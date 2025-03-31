de4e6075adbc54cb4dfd45828375c401a9263ace
Alberto Imery <albertoimery8@gmail.com>
2025-03-30 23:58:00 -0500

    added restart functionality, added loading state, sorted displayed results by eta, added more mobile responsive styles

--- app/Results/page.js
+++ app/Results/page.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react';
 import styles from "../page.module.css";
 import {Navbar} from '@/components';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faCar, faLocationArrow, faLocationCrosshairs} from '@fortawesome/free-solid-svg-icons';
+import {faCar, faLocationArrow, faLocationCrosshairs, faSpinner, faRotateRight} from '@fortawesome/free-solid-svg-icons';
 import AutoComplete from './AutoComplete';
 
 
@@ -37,32 +37,34 @@ const Results = () => {
     const [location, setLocation] = useState("");
     const [coords, setCoords] = useState(null);
     const [hovLanes, setHovLanes] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const handleUserLocation = () => {
+        setLoading(true);
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((position) => {
                 const coords = {latitude: position.coords.latitude, longitude: position.coords.longitude};
                 setCoords(coords); // Ensure only the string is stored
                 console.log("📍 User Coordinates:", coords);
+                setLoading(false);
             });
         } else {
             alert("Geolocation is not supported by this browser.");
+            setLoading(false)
         }
     };
     
 
     /* Triggers fetchHovLanes() when location or coords is provided */
     useEffect(() => {
-        if (location) {
-            fetchHovLanes();
-        }
-        else if (coords) {
+        if (location || coords) {
             fetchHovLanes();
         }
     }, [location, coords]);
 
     /* Gets HOV lane results */
     const fetchHovLanes = async () => {
+        setLoading(true);
         console.log("Sending request with:", { location, coords });
     
         try {
@@ -80,8 +82,15 @@ const Results = () => {
         } catch (error) {
             console.error("Fetch Error:", error);
         }
+        setLoading(false);
     };
     
+    /* Sort HOV Lane results by ETA to display in order */
+    const sortedLanes = hovLanes.sort((a, b) => {
+        const etaA = parseInt(a.eta.replace(/\D/g, "")); 
+        const etaB = parseInt(b.eta.replace(/\D/g, ""));
+        return etaA - etaB; 
+    });
     
 
     return (
@@ -91,30 +100,56 @@ const Results = () => {
             </div>
 
             <div className={styles.resultsPageSpacing}>
+                {/* Hides location input elements when results are ready */}
+                {hovLanes.length === 0 && (
+                   <>
 
-
-                <AutoComplete onClick={fetchHovLanes} setLocation={setLocation}/>
+                    <AutoComplete onClick={fetchHovLanes} setLocation={setLocation}/>
                 
+                    <div className={styles.centerOrDiv}>
+                        <h3 className={styles.orText}>
+                            <span className={styles.orSpan}>OR</span>
+                        </h3>
+                    </div>
 
-                <div className={styles.centerOrDiv}>
-                    <h3 className={styles.orText}>
-                        <span className={styles.orSpan}>OR</span>
-                    </h3>
-                </div>
-
-                <button
-                 className={styles.currentLocBtn}
-                 onClick={handleUserLocation}>
-                    USE CURRENT LOCATION
-                    <FontAwesomeIcon icon={faLocationCrosshairs} />
-                </button>
+                    <button
+                    className={styles.currentLocBtn}
+                    onClick={handleUserLocation}>
+                        USE CURRENT LOCATION
+                        <FontAwesomeIcon icon={faLocationCrosshairs} />
+                    </button>
+                   </>
+                )} 
             </div>
+            
+            {/* Renders loading icon */}
+            {loading && (
+                <div className={styles.loader}>
+                    <FontAwesomeIcon icon={faSpinner} className={styles.spinnerIcon} />
+                    <p>Getting Your Closest HOV Lanes...</p>
+                </div>
+            )}
 
             <div className={styles.hovResults}>
-                {hovLanes.map((lane, index) => (
+            
+            {/* Renders Restart button */}
+            {hovLanes.length > 0 && (
+                <button
+                    className={styles.restartBtn}
+                    onClick={() => {
+                        setLocation("");
+                        setCoords(null);
+                        setHovLanes([]);
+                    }}
+                >
+                    Restart Search
+                    <FontAwesomeIcon icon={faRotateRight} />
+                </button> 
+            )}
+                {/* Renders result cards */}
+                {sortedLanes.map((lane, index) => (
                 <HovLaneCard key={index} lane={lane} /> 
                 ))}
- 
             </div>
 
         </div>
