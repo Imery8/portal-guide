446fa33add69f744a1d401b739b18d7baab8cf1e
Alberto Imery <albertoimery8@gmail.com>
2025-04-05 19:50:30 -0500

    home page button animation change, added emails to feedback link

--- components/FeedbackLink.jsx
+++ components/FeedbackLink.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from "../app/page.module.css";
 
 const FeedbackLink = () => {
-    const email = "imerya3638@uhcl.edu,gichanas3673@uhcl.edu,mcgowant5347@uhcl.edu";
+    const email = "imerya3638@uhcl.edu,gichanas3673@uhcl.edu,mcgowant5347@uhcl.edu,denisons1509@uhcl.edu,wierl9428@uhcl.edu";
     const subject = encodeURIComponent("HOV Locator user test comments / suggestions");
   
     return (
